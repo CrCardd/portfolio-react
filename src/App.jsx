94af488a893e3b5c1c53beb1e01aed8208d6f48a
CrCardd <cris.rc377@gmail.com>
2024-09-16 15:06:07 -0300

    inicializando a apresentação resumida

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
+// import { useState } from 'react'
 import Menu from './components/Menu.jsx'
+import style from './App.module.css'
 
 function App() {
 
   return (
     <>
       <Menu/>
+      <div className={style.resume}>
+
+        <div className={style.apresentation}>
+          <h1>Soy Cristian</h1>
+          <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Blanditiis ducimus recusandae aliquam omnis a cum ex at repellendus</p>
+        </div>
+
+      </div>
     </>
   )
 }
